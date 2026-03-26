9d300a4a48450478365ceaf6a7716844d2ecf37e
delz012 <delz@sanditel.id>
2026-03-26 21:30:28 +0700

    add dude repo

--- terraform/ecr.tf
+++ terraform/ecr.tf
@@ -89,3 +89,49 @@ module "ecr_frontend" {
     Environment = var.environment
   }
 }
+
+module "dude_bot" {
+  source  = "terraform-aws-modules/ecr/aws"
+  version = "~> 2.0"
+
+  repository_name = "${var.project_name}-dude"
+  repository_type = "private"
+  repository_force_delete = true
+
+  repository_image_scan_on_push = true
+
+  repository_lifecycle_policy = jsonencode({
+    rules = [
+      {
+        rulePriority = 1,
+        description  = "Keep last 10 production images",
+        selection = {
+          tagStatus     = "tagged",
+          tagPrefixList = ["v"],
+          countType     = "imageCountMoreThan",
+          countNumber   = 10
+        },
+        action = {
+          type = "expire"
+        }
+      },
+      {
+        rulePriority = 2,
+        description  = "Expire untagged images older than 1 days",
+        selection = {
+          tagStatus   = "untagged",
+          countType   = "sinceImagePushed",
+          countUnit   = "days",
+          countNumber = 1
+        },
+        action = {
+          type = "expire"
+        }
+      }
+    ]
+  })
+
+  tags = {
+    Environment = var.environment
+  }
+}

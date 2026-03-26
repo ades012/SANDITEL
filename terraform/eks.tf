b61e0d69ba640e435805972aa024ba44e0d13db9
delz012 <delz@sanditel.id>
2026-03-27 00:29:56 +0700

    rollback to 1.29

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -3,7 +3,7 @@ module "eks" {
   version = "~> 20.0"
 
   cluster_name    = "${var.project_name}-eks"
-  cluster_version = "1.33"
+  cluster_version = "1.29"
 
   vpc_id                   = module.vpc.vpc_id
   subnet_ids               = module.vpc.private_subnets
@@ -40,7 +40,7 @@ module "eks" {
       max_size     = 3
       desired_size = 2
 
-      instance_types = ["t3.medium"]
+      instance_types = ["t3.micro"]
       ami_type       = "AL2_x86_64"
     }
   }

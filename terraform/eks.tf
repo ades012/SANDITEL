75c65c9dc2f2d42f2f4e87f4db13f1b8cbc0d0ce
delz012 <delz@sanditel.id>
2026-03-30 08:44:37 +0700

    change version kubernetes to 1.31 & instance to t3.medium

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -3,7 +3,7 @@ module "eks" {
   version = "~> 20.0"
 
   cluster_name    = "${var.project_name}-eks"
-  cluster_version = "1.29"
+  cluster_version = "1.31"
 
   vpc_id                   = module.vpc.vpc_id
   subnet_ids               = module.vpc.private_subnets
@@ -15,24 +15,24 @@ module "eks" {
   enable_irsa                              = true
   enable_cluster_creator_admin_permissions = true
 
-  node_security_group_additional_rules = {
-    wireguard_outbound = {
-      description      = "Allow WireGuard outbound to Office"
-      protocol         = "udp"
-      from_port        = 51820
-      to_port          = 51820
-      type             = "egress"
-      cidr_blocks      = ["0.0.0.0/0"]
-    }
-    wireguard_inbound = {
-      description      = "Allow WireGuard inbound for handshake"
-      protocol         = "udp"
-      from_port        = 51820
-      to_port          = 51820
-      type             = "ingress"
-      cidr_blocks      = ["0.0.0.0/0"]
-    }
-  }
+  # node_security_group_additional_rules = {
+  #   wireguard_outbound = {
+  #     description      = "Allow WireGuard outbound to Office"
+  #     protocol         = "udp"
+  #     from_port        = 51820
+  #     to_port          = 51820
+  #     type             = "egress"
+  #     cidr_blocks      = ["0.0.0.0/0"]
+  #   }
+  #   wireguard_inbound = {
+  #     description      = "Allow WireGuard inbound for handshake"
+  #     protocol         = "udp"
+  #     from_port        = 51820
+  #     to_port          = 51820
+  #     type             = "ingress"
+  #     cidr_blocks      = ["0.0.0.0/0"]
+  #   }
+  # }
 
   eks_managed_node_groups = {
     worker_nodes = {
@@ -40,7 +40,7 @@ module "eks" {
       max_size     = 3
       desired_size = 2
 
-      instance_types = ["t3.micro"]
+      instance_types = ["t3.medium"]
       ami_type       = "AL2_x86_64"
     }
   }

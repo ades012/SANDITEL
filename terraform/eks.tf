e6af750c9e8a10cf1a2b8c0e9f12831a464ad454
delz012 <delz@sanditel.id>
2026-03-30 10:10:25 +0700

    rollback instance type to t3.micro

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -40,7 +40,7 @@ module "eks" {
       max_size     = 3
       desired_size = 2
 
-      instance_types = ["t3.medium"]
+      instance_types = ["t3.micro"]
       ami_type       = "AL2_x86_64"
     }
   }
